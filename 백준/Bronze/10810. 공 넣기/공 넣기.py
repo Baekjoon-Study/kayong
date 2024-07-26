f46148fce070640714df4c3ffb2c01e627dc6f07
Ka-yong <kayong.jung@gmail.com>
2024-07-26 12:18:12 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,8 @@
+N, M = map(int, input().split())
+A = [0]*N
+for p in range(M):
+    i, j, k = map(int, input().split())
+    for q in range(i-1, j):
+        A[q] = k
+for i in A:
+    print(i, end=' ')
