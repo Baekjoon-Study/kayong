18ee454dc0dc8125e4ffda0e9ce76bf85b972efe
Ka-yong <kayong.jung@gmail.com>
2024-09-02 13:27:35 +0900

    [Bronze I] Title: 약수들의 합, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,18 +1,14 @@
-n = int(input())
-while n != -1:
-    A = []                               #반복문에 변수 초기화 유념
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    A = []
     S = 0
     for i in range(1, int(n/2)+1):
         if n%i == 0:
             A.append(i)
             S += i
     if S == n:
-        print('%s = '%n, end='')
-        for i in A:
-            if i != A[len(A)-1]:
-                print('%s + '%i, end='')
-            else:
-                print(i)
+        print(n, ' = ', ' + '.join(str(i) for i in A), sep='')
     else:
         print('%s is NOT perfect.'%n)
-    n = int(input())
